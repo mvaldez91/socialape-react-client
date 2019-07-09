ed8ca37ce0facd4e8373bfae3a7a8fdf48957b30
Marco <mtulio1991@live.com>
2019-07-08 22:10:27 -0600

    Login page

--- src/pages/home.js
+++ src/pages/home.js
@@ -13,7 +13,7 @@ class Home extends Component {
         fetch('/scream',
             {
                 headers:{
-                    'Authorization': 'Bearer eyJhbGciOiJSUzI1NiIsImtpZCI6Ijc0MWRlNGY0NTMzNzg5YmRiMjUxYjdhNTgwNTZjNTZmY2VkMjE0MWIiLCJ0eXAiOiJKV1QifQ.eyJpc3MiOiJodHRwczovL3NlY3VyZXRva2VuLmdvb2dsZS5jb20vcmVhY3Qtc29jaWFsLW5ldHdvcmstMmU3YWEiLCJhdWQiOiJyZWFjdC1zb2NpYWwtbmV0d29yay0yZTdhYSIsImF1dGhfdGltZSI6MTU2MjY0MDc3OSwidXNlcl9pZCI6Ik1BeVA3UzNNa0RoSWc2T09sZmhyUXpHQzZyazIiLCJzdWIiOiJNQXlQN1MzTWtEaElnNk9PbGZoclF6R0M2cmsyIiwiaWF0IjoxNTYyNjQwNzc5LCJleHAiOjE1NjI2NDQzNzksImVtYWlsIjoibXR1bGlvMTk5MUBsaXZlLmNvbSIsImVtYWlsX3ZlcmlmaWVkIjpmYWxzZSwiZmlyZWJhc2UiOnsiaWRlbnRpdGllcyI6eyJlbWFpbCI6WyJtdHVsaW8xOTkxQGxpdmUuY29tIl19LCJzaWduX2luX3Byb3ZpZGVyIjoicGFzc3dvcmQifX0.Im2FoYN1zsXyayoJWprvB9VVMlD4JCmAvvUFVob-0Nj6jpIOiNZAtmsDXqCzZQ0sxkWzYsGrfsAdVgY6vWhXwpt3LqEd5hBTJYhgyuOcNqAFCiSwqxAHorD_7VjsLM81gUEO0ZHHPDClvkYkhCxarvefwwGR_SgQJSGAg23DO3LxvSgDoWMrd53c_fBW6cVbsEbYYsfzg4Y9EUtRG2VIKGmnNDwSvWn6aoEL9TMto8iCpbU3NABAugayB7ypJfk8ldzDKoj3sv61Qy5721rIvmfasCzBlcRGsgu5h5itmHdW-9WflTWSwuC55ZoRBj2Bx3alt_w4zZJUzkt_9oqRsA'
+                    'Authorization': ''
                 }
             })
             .then(res=>res.json())
--- src/pages/login.js
+++ src/pages/login.js
@@ -1,24 +1,123 @@
 import React, {Component} from 'react';
 import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
+import AppIcon from '../images/monkey_2.png'
 
+import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
 
 const styles ={
     form: {
         textAlign: 'center'
+    },
+    image: {
+        margin: '20px auto 20px auto'
+    },
+    pageTitle: {
+        margin: '10px auto 10px auto'
+    },
+    textField:{
+        margin: '10px auto 10px auto'
+    },
+    button: {
+        marginTop: 20
     }
 };
+
+
 class Login extends Component {
+    constructor(props){
+        super(props);
+        this.state = {
+            email: '',
+            password: '',
+            loading: false,
+            errors:{}
+        }
+    }
+
+    handleSubmit = (event)=>{
+        let resStatus = 0;
+        event.preventDefault();
+        this.setState({
+            loading: true
+        });
+        const userData = {
+            email: this.state.email,
+            password: this.state.password
+        };
+        fetch('/login', {
+            method: 'POST',
+            headers:{
+                "Accept":"application/json",
+                "Content-Type":"application/json"
+            },
+            body: JSON.stringify(userData)})
+            .then(res=> {
+                resStatus = res.status;
+                return res.json()
+            })
+            .then(data=>{
+                if (resStatus >= 400){
+                    throw JSON.stringify(data);
+                }
+                this.setState({
+                    loading:false
+                });
+                this.props.history.push('/')
+            }).catch(err=>{
+                console.log(err)
+            this.setState({
+                errors: JSON.parse(err),
+                loading: false
+            })
+        })
+    };
+
+
+    handleChange = (event) =>{
+      this.setState({
+         [event.target.name]: event.target.value
+      });
+    };
     render(){
         const {classes} = this.props;
+        const {errors, loading} = this.state;
 
         return(
           <Grid container className={classes.form}>
             <Grid item sm>
 
             </Grid>
-            <Grid item sm></Grid>
+            <Grid item sm>
+                <img src={AppIcon} alt="monkey" className={classes.image} />
+                <Typography variant="h3" className={classes.pageTitle} >
+                    Login
+                </Typography>
+                <form noValidate onSubmit={this.handleSubmit}>
+                    <TextField id="email"
+                               name="email"
+                               type="email"
+                               label="Email"
+                               className={classes.textField}
+                               value={this.state.email}
+                               helperText={errors.email}
+                               error={errors.email ? true: false}
+                               onChange={this.handleChange} fullWidth/>
+                    <TextField id="password"
+                               name="password"
+                               type="password"
+                               label="Password"
+                               className={classes.textField}
+                               value={this.state.password}
+                               helperText={errors.password}
+                               error={errors.password ? true: false}
+                               onChange={this.handleChange} fullWidth/>
+                    <Button type="submit" variant="contained" color="primary" className={classes.button}>Login</Button>
+                </form>
+            </Grid>
             <Grid item sm></Grid>
           </Grid>
         );
