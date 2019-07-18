4db21308440422e98e82ab6f6b1bd2c65e75b274
Marco <mtulio1991@live.com>
2019-07-17 18:50:51 -0600

    Devtools only in dev environment for redux store

--- src/redux/store.js
+++ src/redux/store.js
@@ -16,11 +16,18 @@ const reducers = combineReducers({
     UI: uiReducer
 });
 
-const store = createStore(reducers,initialState, 
-    compose(
-        applyMiddleware(...middleware), 
+
+var store = {};
+
+if(process.env.NODE_ENV === 'production') {
+    store = createStore(reducers, initialState, compose(
+        applyMiddleware(...middleware)
+    ));
+} else {
+    store = createStore(reducers, initialState, compose(
+        applyMiddleware(...middleware),
         window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__()
-        )
-    );
+    ));
+}
 
 export default store;
